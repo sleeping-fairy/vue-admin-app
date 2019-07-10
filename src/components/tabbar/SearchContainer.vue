e3439fb8722d0e6f002bb2b2f8941565ee0cd3e4
sleeping-fairy <nie_fairy@163.com>
2019-07-10 16:25:59 +0800

    first commit v1.0

---
+++ src/components/tabbar/SearchContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>SearchContainer</h3>
+  </div>
+</template>
+
+<script>
+import { Toast } from 'mint-ui';
+
+  export default {
+    created() {
+      Toast({
+        message: '请求服务器失败',
+        position: 'bottom',
+        duration: 5000
+      });
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
